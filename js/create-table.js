ab484a47a6695d4b93f4ed15aa73f6df2361a4f2
Gobain <joaopedrogobain@gmail.com>
2024-09-12 16:52:03 -0300

    - Adding buttons to buy GPUs.

--- js/create-table.js
+++ js/create-table.js
@@ -19,16 +19,28 @@ async function createTable() {
 
                 function createCellWithClassAndContent(content, className) {
                     const cell = tr.insertCell();
-                    cell.textContent = content;
+                    cell.textContent = content[0];
+                    if (content[1] !== undefined) {
+                        const newLink = document.createElement('a');
+                        const newButton = document.createElement('button');
+                        newLink.href = content[1];
+                        newLink.classList.add("buy-link");
+                        newButton.textContent = 'COMPRAR';
+
+                        newButton.classList.add("buy-button");
+
+                        newLink.appendChild(newButton);
+                        cell.appendChild(newLink);
+                    }
                     cell.classList.add(className);
                 }
 
-                createCellWithClassAndContent(gpu[i].name, (gpu[i].brand + "-line"));
-                createCellWithClassAndContent(gpu[i].fps_1080, "gpu-data");
-                createCellWithClassAndContent(gpu[i].fps_1440, "gpu-data");
-                createCellWithClassAndContent(gpu[i].fps_2160, "gpu-data");
-                createCellWithClassAndContent(BRL.format(gpu[i].price), "gpu-data");
-                createCellWithClassAndContent((gpu[i].price / gpu[i].fps_1080).toFixed(2), "gpu-data");
+                createCellWithClassAndContent([gpu[i].name, gpu[i].link], (gpu[i].brand + "-line"));
+                createCellWithClassAndContent([gpu[i].fps_1080], "gpu-data");
+                createCellWithClassAndContent([gpu[i].fps_1440], "gpu-data");
+                createCellWithClassAndContent([gpu[i].fps_2160], "gpu-data");
+                createCellWithClassAndContent([BRL.format(gpu[i].price)], "gpu-data");
+                createCellWithClassAndContent([(gpu[i].price / gpu[i].fps_1080).toFixed(2)], "gpu-data");
 
 
             }
